771710cf6d0986be71d2becfa7a3106fe0191e3a
angel prats <angelpratz@gmail.com>
2019-05-28 11:47:04 -0400

    counter is incrementing and decramenting, project complete

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,40 +3,40 @@ import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
 
 class Counter extends Component {
-    incrementIfOdd = () => {
-        // Stretch Problem: Implement an increment function that
-        // only increments if the counter value is odd
-    };
+  incrementIfOdd = () => {
+    // Stretch Problem: Implement an increment function that
+    // only increments if the counter value is odd
+  };
 
-    incrementAsync = () => {
-        // Stretch Problem: Implement an increment function that
-        // increments after waiting for one second
-    };
+  incrementAsync = () => {
+    // Stretch Problem: Implement an increment function that
+    // increments after waiting for one second
+  };
 
-    render() {
-        // Fill in the two button onClick methods
-        // Upon clicking these buttons, the count
-        // should decrement or increment accordingly
-        return (
-            <p>
-                Clicked: {this.props.count} times
-                <button onClick={() => {/* Fill me in */ }}>
-                    +
+  render() {
+    // Fill in the two button onClick methods
+    // Upon clicking these buttons, the count
+    // should decrement or increment accordingly
+    return (
+      <p>
+        Clicked: {this.props.count} times
+                <button onClick={() => { this.props.increment() }}>
+          +
                 </button>
-                <button onClick={() => {/* Fill me in */ }}>
-                    -
+        <button onClick={() => { this.props.decrement() }}>
+          -
                 </button>
-                 {/* Uncomment these button tags if you got
+        {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+        {/* <button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
                 </button>  */}
-            </p>
-        );
-    }
+      </p>
+    );
+  }
 }
 
 // The mapStateToProps function specifies which portion of the
@@ -46,9 +46,9 @@ class Counter extends Component {
 // redux application, though, it would receive only the relevant
 // parts it needs from the state object.
 const mapStateToProps = (state) => {
-    return {
-        count: state.count
-    };
+  return {
+    count: state.count
+  };
 };
 
 // The connect function is called in order to make this component aware
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,13 +9,15 @@ const initialState = {
 // of our redux store, along with an action created
 // by our action creator. What does the reducer
 // need to do with the count in each case?
-export default (count = 0, action) => {
+export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
+      return { ...state, count: state.count + 1 };
     // Fill in the body of this case
     case DECREMENT:
-    // Fill in the body of this case
+      // Fill in the body of this case
+      return { ...state, count: state.count - 1 };
     default:
-      return ++count;
+      return state;
   }
 };
